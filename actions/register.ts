6a53bfe0a4a13fff751b566652a90dcb2fc5c790
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-12 08:17:06 +0600

    Register Functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,10 +1,30 @@
 "use server";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { hash } from "bcrypt";
 import * as z from "zod";
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validate = RegisterSchema.safeParse(values);
   if (!validate.success) {
-    return { error: "Register Success" };
+    return { error: "Validation Failed" };
   }
+
+  const { name, email, password } = validate.data;
+  const user = await getUserByEmail(email);
+  if (user) {
+    return { error: "Email already in user!" };
+  }
+
+  const hashedPassword = await hash(password, 10);
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+  //Todo: email verification
   return { success: "Register Success" };
 };
