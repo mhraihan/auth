a691f0b1fb48fd801abb936352163c0c492507ef
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-22 18:48:05 +0600

    any type resolve

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,7 +2,10 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 const from = `${process.env.MAIL_TITLE} <${process.env.MAIL_FROM}>`;
-export const sendVerificationEmail = async (email: string, token: string) => {
+export const sendVerificationEmail = async (
+  email: string,
+  token: string
+): Promise<boolean> => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   await resend.emails.send({
     from: `${from}`,
@@ -10,9 +13,13 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     subject: "Verification email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
+  return true;
 };
 
-export const sendPasswordResetEmail = async (email: string, token: string) => {
+export const sendPasswordResetEmail = async (
+  email: string,
+  token: string
+): Promise<boolean> => {
   const confirmLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
     from: `${from}`,
@@ -20,13 +27,18 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     subject: "Password reset email",
     html: `<p>Click <a href="${confirmLink}">here</a> to reset password.</p>`,
   });
+  return true;
 };
 
-export const sendTwoFactorEmail = async (email: string, token: string) => {
+export const sendTwoFactorEmail = async (
+  email: string,
+  token: string
+): Promise<boolean> => {
   await resend.emails.send({
     from: `${from}`,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA Code ${token} </p>`,
   });
+  return true;
 };
